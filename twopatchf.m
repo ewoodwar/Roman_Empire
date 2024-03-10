b61dd7bb548ba20b50480ef38a6b1792e0681e94
ewoodwar <71281174+ewoodwar@users.noreply.github.com>
2024-03-10 13:01:21 -0400

    Update twopatchf.m
    Updated code

--- twopatchf.m
+++ twopatchf.m
@@ -9,8 +9,7 @@ alpha_21=0.2;
 omega=0.2;
 gamma_12=0.3;
 gamma_21=0.3;
-sigma_12=0.4;
-sigma_21=0.4;
+sigma=0.4;
 mu=75;
 
 %p(1)= S1
@@ -20,9 +19,9 @@ mu=75;
 %p(5)= R1
 %p(6)= R2
 
-pdot(1,:)=pi_1-beta_1*p(1).*p(3)-alpha_12*p(1)+omega*p(5);
-pdot(2,:)=pi_2 - ((beta_2*p(2))/(p(4)+p(2)))+ alpha_12*p(1) - alpha_21*p(2)+ omega*p(6);
-pdot(3,:)= beta_1*p(2)+ gamma_21*p(4)-gamma_12*p(3)-sigma_12*p(4);
-pdot(4,:)=beta_2*p(2)-gamma_21*p(4)+ gamma_12*p(3) - sigma_21*p(4);
-pdot(5,:)=mu*p(3)-alpha_12*p(5)+alpha_21*p(6)-omega*p(5);
-pdot(6,:)=mu*p(4)+alpha_12*p(5)-alpha_21*p(6)-omega*p(6);
+pdot(1,:)=pi_1-(beta_1.*p(1).*p(3))-alpha_12.*p(1)+omega.*p(5);
+pdot(2,:)=pi_2 - ((beta_2.*p(2).*p(3))/(p(4)+p(2)+p(6)))+ alpha_12.*p(1) - alpha_21.*p(2)+ omega*p(6);
+pdot(3,:)= beta_1.*p(2)+ gamma_21.*p(4)-gamma_12.*p(3)-sigma.*p(3)-mu.*p(3);
+pdot(4,:)=beta_2.*p(2)-gamma_21.*p(4)+ gamma_12.*p(3) - sigma.*p(4)-mu.*p(4);
+pdot(5,:)=mu.*p(3)-alpha_12.*p(5)+alpha_21.*p(6)-omega.*p(5);
+pdot(6,:)=mu.*p(4)+alpha_12.*p(5)-alpha_21.*p(6)-omega.*p(6);
